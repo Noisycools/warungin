1540857d3ad67cca6b663bcfc172d1b09c8b7bb9
aditya-cg <adityacg534@gmail.com>
2022-04-07 11:31:23 +0700

    update laporan harian

--- app/Controllers/Admin.php
+++ app/Controllers/Admin.php
@@ -2,8 +2,23 @@
 
 namespace App\Controllers;
 
+
+use App\Models\TransactionModel;
+use App\Models\BarangTransaksiModel;
+use App\Models\AdminModel;
+use CodeIgniter\I18n\Time;
+
 class Admin extends BaseController
 {
+    protected $transaksiModel;
+    protected $barangTransaksiModel;
+    public function __construct()
+    {
+        $this->transaksiModel = new TransactionModel();
+        $this->barangTransaksiModel = new BarangTransaksiModel();
+        $this->adminModel = new AdminModel();
+    }
+
     public function index()
     {
         $data = ['title' => "Admin | Dashboard"];
@@ -12,8 +27,19 @@ class Admin extends BaseController
 
     public function lap_harian()
     {
-        $data = ['title' => "Admin | Dashboard"];
-        return view('admin/', $data);
+        $myTime = Time::now('Asia/Jakarta');
+        $date = $myTime->toLocalizedString('d MMM yyyy');
+        $tanggal0 = $this->request->getPost('tanggal');
+        $tanggal = date('d M Y', strtotime($tanggal0));
+        // dd($tanggal);
+        $data = [
+            'title' => "Admin | Dashboard",
+            'tanggal' => $tanggal,
+            'date' => $date,
+            'transaksi' => $this->adminModel->lap_harian($tanggal),
+            'jumlah' => $this->adminModel->jumlah($tanggal)
+        ];
+        return view('admin/lap_harian', $data);
     }
 
     public function lap_mingguan()
--- app/Views/admin/index.php
+++ app/Views/admin/index.php
@@ -31,7 +31,6 @@
           <div class="small-box bg-info">
             <div class="inner">
               <h3>8</h3>
-
               <p>New Orders</p>
             </div>
             <div class="icon">
@@ -46,7 +45,6 @@
           <div class="small-box bg-success">
             <div class="inner">
               <h3>60<sup style="font-size: 20px">%</sup></h3>
-
               <p>Bounce Rate</p>
             </div>
             <div class="icon">
@@ -61,7 +59,6 @@
           <div class="small-box bg-warning">
             <div class="inner">
               <h3>4</h3>
-
               <p>User Registrations</p>
             </div>
             <div class="icon">
@@ -76,7 +73,6 @@
           <div class="small-box bg-danger">
             <div class="inner">
               <h3>16</h3>
-
               <p>Unique Visitors</p>
             </div>
             <div class="icon">
@@ -93,18 +89,20 @@
             </div>
             <div class="card-body">
               <!-- Date -->
-              <div class="form-group">
-                <label>Laporan Tanggal:</label>
-                <div class="row">
-                  <div class="input-group date" id="reservationdate" data-target-input="nearest" style="width: 80%;">
-                    <input type="text" class="form-control datetimepicker-input" data-target="#reservationdate" />
-                    <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
-                      <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+              <form action="admin/lap_harian" method="POST">
+                <div class="form-group">
+                  <label>Laporan Tanggal:</label>
+                  <div class="row">
+                    <div class="input-group date" id="reservationdate" data-target-input="nearest" style="width: 80%;">
+                      <input type="text" class="form-control datetimepicker-input" data-target="#reservationdate" name="tanggal" />
+                      <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
+                        <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                      </div>
                     </div>
+                    &ensp;<button type="submit" class="btn btn-block btn-primary" style="width: 15%;"><i class="fa fa-download"></i></button>
                   </div>
-                  &ensp;<button type="button" class="btn btn-block btn-primary" style="width: 15%;"><i class="fa fa-download"></i></button>
                 </div>
-              </div>
+              </form>
               <!-- Date range -->
               <div class="form-group">
                 <label>Laporan mingguan:</label>
